12902a0392f77a3a960ab0637e90c3669c5c2b56
lblommesteyn <lblommes@uwo.ca>
2024-02-02 20:39:51 -0500

    adding in ivey text from document notes

--- src/app/info/services/page.tsx
+++ src/app/info/services/page.tsx
@@ -17,7 +17,52 @@ export default function ServicesPage() {
 
         <br/>
 
-        
+        <p>
+          CHEER Works employs members of the CHEER Group who have been
+          developing their job skills. This is a safe and assisted working
+          environment providing paid employment for our community members with
+          intellectual disabilities. Caregivers and community supporters
+          volunteer to help with this initiative. There are many different jobs
+          to be done. Everyone enjoys working together, and we have a great
+          team!
+        </p>
+
+        <br/>
+
+        <h1>Cheer Connections</h1>
+        <p>
+          Cheer Connections is a group of parents and caregivers, some have
+          adult children in the CHEER group and some don’t, but we are all in a
+          similar situation. We meet at least once a month to offer each other
+          support and share our knowledge. Our winter meetings were funded by
+          the Ontario Caregivers Association, which provided a relaxing day, a
+          nice lunch, and great guest speakers.
+        </p>
+        <br/>
+
+        <p>
+          The Cheer Connections regularly gather for various workshops so that
+          we may discuss information and learn together. We are all concerned
+          about ODSP, housing, employment, social opportunities, etc., and a lot
+          of our energy is given to finding solutions for our loved ones’
+          future. This group helps reduce isolation for caregivers as well. It
+          is a requirement of the CHEER Group that family members become
+          involved with Cheer Connections. We are a close-knit social group that
+          includes siblings, friends, and neighbors who care about someone with
+          an intellectual disability. We are committed to developing a community
+          of inclusion. We are supported by Ontario Caregivers Association,
+          Algarva 168 (Alhambra), and private community member donors. We also
+          run various fundraisers, and donations are accepted online through
+          Canada Helps.
+        </p>
+
+        <br/>
+
+        <p>
+          Looking for some social fun?… we have that too! Respite care is also
+          available so you don’t have to worry about your loved one while you
+          attend meetings.
+        </p>
       </div>
     </>
   );
