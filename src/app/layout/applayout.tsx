d3c7ade684c171816f4ebd68dd6994e40ddaadb2
Kylockwan03 <kylockwan@gmail.com>
2024-02-10 12:23:00 -0500

    app layout stuff

---
+++ src/app/layout/applayout.tsx
@@ -0,0 +1,33 @@
+import Root from React;
+import loader from UseLoader;
+
+export default function AppWithProviders() {
+	const data = useLoaderData<typeof loader>();
+	return (
+		<ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+			<App />
+		</ThemeProvider>
+	);
+}
+
+export function App() {
+	const data = useLoaderData<typeof loader>();
+	const [theme] = useTheme();
+	return (
+		<html lang="en" className={clsx(theme)}>
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
+				<Links />
+			</head>
+			<body>
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
+}
